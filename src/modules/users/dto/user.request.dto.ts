bcffa4926e05708703bb331c325376cd366c54b3
Brayan Guerrero <brayanstewartguor@gmail.com>
2024-07-01 15:15:27 -0500

    - Implement IsValid decorator

--- src/modules/users/dto/user.request.dto.ts
+++ src/modules/users/dto/user.request.dto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from "class-validator";
 
 export class UserRequestDto {
     @IsNotEmpty({message: 'Email is required'})
     @IsString({message: 'Email must be a string'})
     @MaxLength(50, {message: 'Email must be a maximum of 50 characters'})
+    @IsEmail({}, {message: 'Email must be a valid email address'})
     email: string;
 
     @IsNotEmpty({message: 'Password is required'})
